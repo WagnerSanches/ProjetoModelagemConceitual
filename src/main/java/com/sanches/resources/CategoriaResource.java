2bb4b89fbc5a144fc08929fbc5bbc50b4e11306f
wagnerSanches <wagnersanches5@hotmail.com>
2021-06-13 12:50:29 -0300

    criando service e repository Categoria

--- src/main/java/com/sanches/resources/CategoriaResource.java
+++ src/main/java/com/sanches/resources/CategoriaResource.java
@@ -3,15 +3,22 @@ package com.sanches.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sanches.domain.Categoria;
+import com.sanches.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
@@ -23,6 +30,12 @@ public class CategoriaResource {
 		lista.add(cat2);
 		
 		return lista;
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+		Categoria cat = service.buscar(id);
+		return ResponseEntity.ok().body(cat);
 	}
 	
 }
