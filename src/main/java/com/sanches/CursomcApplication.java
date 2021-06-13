fac9d2d6dee1e7ce5977fe05dd9f39adafdd80a1
wagnerSanches <wagnersanches5@hotmail.com>
2021-06-13 14:21:17 -0300

    criando instancicoes de iniciacao

--- src/main/java/com/sanches/CursomcApplication.java
+++ src/main/java/com/sanches/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.sanches;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.sanches.domain.Categoria;
+import com.sanches.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository repositoryCategoria;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		repositoryCategoria.saveAll(Arrays.asList(cat1, cat2));
+	
+	}
 }
