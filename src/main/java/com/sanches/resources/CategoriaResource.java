5e24d77eee20119f81de2ad927479cdaf74f7891
wagnerSanches <wagnersanches5@hotmail.com>
2021-06-13 16:56:50 -0300

    criando exception

--- src/main/java/com/sanches/resources/CategoriaResource.java
+++ src/main/java/com/sanches/resources/CategoriaResource.java
@@ -35,7 +35,9 @@ public class CategoriaResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> buscar(@PathVariable Integer id) {
 		Categoria cat = service.buscar(id);
+
 		return ResponseEntity.ok().body(cat);
 	}
+	
 	
 }
